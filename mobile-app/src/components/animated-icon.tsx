3744968e2986c5aef9ab282ea59b17c98de9ec3a
AkshaySingare <singareakshay937@gmail.com>
2026-08-28 15:11:44 +0530

    removed microphone

--- mobile-app/src/components/animated-icon.tsx
+++ mobile-app/src/components/animated-icon.tsx
@@ -33,7 +33,7 @@ export function AnimatedSplashOverlay() {
     },
   });
 
-  const image = <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />;
+  const image = <Image style={styles.image} source={require('@/assets/images/tich-surksha-woman.png')} />;
 
   return animate ? (
     <Animated.View
@@ -104,7 +104,7 @@ export function AnimatedIcon() {
 
       <Animated.View entering={keyframe.duration(DURATION)} style={styles.background} />
       <Animated.View style={styles.imageContainer} entering={logoKeyframe.duration(DURATION)}>
-        <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />
+        <Image style={styles.image} source={require('@/assets/images/tich-surksha-woman.png')} />
       </Animated.View>
     </View>
   );
